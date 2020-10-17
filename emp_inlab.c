054f9c57e99afefa1a44bb4186bed5043fb6a43c
Nicholas Chrysanthou <Nchrysanthou@myseneca.ca>
2020-10-17 04:13:02 -0400

    Unfinished Assignment

--- emp_inlab.c
+++ emp_inlab.c
@@ -1,44 +1,73 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define SIZE 2
 
 struct Employee
 {
-    int id = 0;
-    int age = 0;
-    double salary = 0.00;
-} emp[SIZE];
-
-enum Options
-{
-    displayInformation = 1,
-    addEmployee = 2,
-    updateSalary = 3,
-    removeEmployee = 4,
-    exit = 0
-} options;
+    int id;
+    int age;
+    double salary;
+} emp[SIZE] = {0};
 
 int main(void)
 {
 
+    bool complete = false;
+    int numEmployees = 0;
+    int option = 0, i = 0;
+
     printf("---=== EMPLOYEE DATA ===---\n\n");
 
-    switch (options)
+    do
     {
-    case displayInformation:
+        printf("1. Display Employee Information\n");
+        printf("2. Add Employee\n");
+        printf("0. Exit\n\n");
+
+        printf("Please select from the above options: ");
+        scanf("%d", &option);
+
+        switch (option)
+        {
+
+        default: // Unknown entry
+            break;
+
+        case 0: // Exit
+            printf("Exiting Employee Data Program. Good Bye!!!");
+            complete = true;
+            break;
+
+        case 1: // Display Function
+            for (int i = 0; i < sizeof(emp); i++)
+                printf("%6d%9d%11.2lf", emp[i].id, emp[i].age, emp[i].salary);
+            printf("\n");
+            break;
+        case 2: // Add Employee
+        {
+            if (i >= sizeof(emp))
+                printf("ERROR!!! Maximum Number Of Employees Reached");
+
+            while (i <= SIZE)
+            {
+                printf("Adding Employee\n");
+                printf("===============\n");
+                printf("Enter Employee ID: ");
+                scanf("%d", &emp[i].id);
+                printf("Enter Employee Age: ");
+                scanf("%d", &emp[i].id);
+                printf("Enter Employee Salary: ");
+                scanf("%.2lf", &emp[i].id);
+                i++;
+                printf("%d", emp[i]);
+            }
+        }
         break;
-    case addEmployee:
-        break;
-    case updateSalary:
-        break;
-    case removeEmployee:
-        break;
-    case exit:
-        printf("Exiting Employee Data Program. Good Bye!!!");
-        return 0;
-    default:
-        break;
-    }
+        }
+
+    } while (!complete);
+
     return 0;
 }
